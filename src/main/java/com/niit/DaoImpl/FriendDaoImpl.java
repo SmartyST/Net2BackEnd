86ad992b0e5c6272f4aa9fb55ee54e453280f9d7
someshturde <someshturde@gmail.com>
2018-03-17 16:21:43 +0530

    Friend Continue.. suggest pending done

---
+++ src/main/java/com/niit/DaoImpl/FriendDaoImpl.java
@@ -0,0 +1,70 @@
+package com.niit.DaoImpl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.niit.dao.FriendDao;
+import com.niit.model.Friend;
+import com.niit.model.User;
+
+@Repository
+@Transactional
+public class FriendDaoImpl implements FriendDao
+{
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public List<User> suggestedUsers(String email) 
+	{
+		Session session=sessionFactory.getCurrentSession();
+		String queryString="select * from UserDetail where email in (select email from UserDetail where email!=? minus (select toId_email from friend where fromId_email=? union select fromId_email from friend where toId_email=?))";
+		SQLQuery query=session.createSQLQuery(queryString);
+		query.setString(0, email);
+		query.setString(1, email);
+		query.setString(2, email);
+		query.addEntity(User.class);
+		List<User> suggestedUsers=query.list();
+		return suggestedUsers;
+	}
+
+	public void addFriend(Friend friend) 
+	{
+		Session session=sessionFactory.getCurrentSession();
+		session.save(friend);
+		
+	}
+
+	public List<Friend> pendingRequests(String toIdEmail) 
+	{
+		Session session=sessionFactory.getCurrentSession();
+		Query query=session.createQuery("from Friend where status=? and toId.email=?");
+		query.setCharacter(0, 'P');
+		query.setString(1, toIdEmail);
+		List<Friend> pendingRequests=query.list();
+		return pendingRequests;
+	}
+
+	public void acceptRequest(Friend request) 
+	{
+		Session session=sessionFactory.getCurrentSession();
+		request.setStatus('A');
+		session.update(request);
+	}
+
+	public void deleteRequest(Friend request) 
+	{
+		Session session=sessionFactory.getCurrentSession();
+		session.delete(request);
+	}
+	
+	
+}
